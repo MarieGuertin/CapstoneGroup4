6bac9b3c636e9afa7e354c1d5d70e22e9170eec4
Emmanuelle Coutu-Nadeau <ecnadeau@tactiohealth.com>
2022-02-09 17:47:33 -0500

    Trying to estimate memory usage

---
+++ speechbrain_slu_timers_and_such/mem_usage.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+from tensorflow import keras
+
+def get_model_memory_usage(batch_size, model):
+    import numpy as np
+    try:
+        from keras import backend as K
+    except:
+        from tensorflow.keras import backend as K
+
+    shapes_mem_count = 0
+    internal_model_mem_count = 0
+    for l in model.layers:
+        layer_type = l.__class__.__name__
+        if layer_type == 'Model':
+            internal_model_mem_count += get_model_memory_usage(batch_size, l)
+        single_layer_mem = 1
+        out_shape = l.output_shape
+        if type(out_shape) is list:
+            out_shape = out_shape[0]
+        for s in out_shape:
+            if s is None:
+                continue
+            single_layer_mem *= s
+        shapes_mem_count += single_layer_mem
+
+    trainable_count = np.sum([K.count_params(p) for p in model.trainable_weights])
+    non_trainable_count = np.sum([K.count_params(p) for p in model.non_trainable_weights])
+
+    number_size = 4.0
+    if K.floatx() == 'float16':
+        number_size = 2.0
+    if K.floatx() == 'float64':
+        number_size = 8.0
+
+    total_memory = number_size * (batch_size * shapes_mem_count + trainable_count + non_trainable_count)
+    gbytes = np.round(total_memory / (1024.0 ** 3), 3) + internal_model_mem_count
+    return gbytes
+
+# create a model
+def create_model():
+  model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+# compile the model
+  model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+  return model
+
+model = create_model()
+model.load_weights("speechbrain_slu_timers_and_such/pretrained_models/EndToEndSLU-eff12bec59b2dc2be4fbc4a27c795bee/model.ckpt")
+get_model_memory_usage(1, model)
